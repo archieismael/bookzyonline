e55dccbbe162bfad0ed2d4f2eadfd77176e4c5b2
student <archie_ismael@yahoo.com>
2021-06-22 23:35:59 +0000

    bookzy initial

---
+++ src/main/java/com/bookzy/demo/bookzyonline/BookzyonlineApplication.java
@@ -0,0 +1,13 @@
+package com.bookzy.demo.bookzyonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzyonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookzy/demo/bookzyonline/BookzyonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookzy.demo.bookzyonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzyonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
